855430e9658dbf0aa17ce530185a39b81764d2d2
Daniel McMullen <dmcmullen@gmail.com>
2019-04-04 12:52:15 -0400

    Homework 10 | Daniel McMullen | spring project structure and update gitignore

---
+++ src/main/java/edu/xavier/csci/romannumeralwebapp/RomanNumeralWebAppApplication.java
@@ -0,0 +1,13 @@
+package edu.xavier.csci.romannumeralwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanNumeralWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomanNumeralWebAppApplication.class, args);
+	}
+
+}
